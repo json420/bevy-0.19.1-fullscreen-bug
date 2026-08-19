4413f8d33e82ac62bb1d4f5184e004717e99db28
Jason Gerard DeRose <jasonderose@gmail.com>
2026-08-19 15:39:32 -0600

    Add enough to trigger bug when you press F11

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{
+        PresentMode, PrimaryWindow, VideoModeSelection, WindowMode, WindowPlugin, WindowResized,
+    },
+};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, toggle_fullscreen)
+        .run();
+}
+
+fn toggle_fullscreen(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    if keyboard.just_pressed(KeyCode::F11) {
+        if let Ok(mut window) = query.single_mut() {
+            window.mode = match window.mode {
+                WindowMode::Windowed => {
+                    WindowMode::Fullscreen(MonitorSelection::Primary, VideoModeSelection::Current)
+                }
+                _ => WindowMode::Windowed,
+            };
+        }
+    }
 }
